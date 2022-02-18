1adee5468b854a6cafb84b67f562ad27e15a7013
Joe <jlaquinte@ustwo.com>
2022-02-18 04:15:04 -0500

    vector math fixes in AvatarFollow script

--- BenchXRSocialExperiments/Assets/ustwo/Scripts/AvatarFollow.cs
+++ BenchXRSocialExperiments/Assets/ustwo/Scripts/AvatarFollow.cs
@@ -5,19 +5,20 @@ using UnityEngine;
 [System.Serializable]
 public class FollowOffset
 {
-    public Transform follow;
-    public Transform target;
-    public Vector3 position;
-    public Vector3 rotation;
+    public Transform vrTarget;
+    public Transform rigTarget;
+    public Vector3 trackingPositionOffset;
+    public Vector3 trackingRotationOffset;
 
     public void Update()
     {
-        target.position = follow.TransformPoint(position);
-        target.rotation = follow.rotation * Quaternion.Euler(rotation);
+        rigTarget.position = vrTarget.TransformPoint(trackingPositionOffset);
+        rigTarget.rotation = vrTarget.rotation * Quaternion.Euler(trackingRotationOffset);
     }
 }
 public class AvatarFollow : MonoBehaviour
 {
+    public float turnSmoothness = 1;
     public FollowOffset head;
     public FollowOffset leftHand;
     public FollowOffset rightHand;
@@ -25,10 +26,16 @@ public class AvatarFollow : MonoBehaviour
     public Transform headConstraint;
     public Vector3 headBodyOffset;
 
+    void Start()
+    {
+        headBodyOffset = transform.position - headConstraint.position;
+    }
+
     void Update()
     {
-        transform.forward = Vector3.ProjectOnPlane(headConstraint.forward, Vector3.up).normalized;
         transform.position = headConstraint.position + headBodyOffset;
+        transform.forward = Vector3.Lerp(transform.forward,
+         Vector3.ProjectOnPlane(headConstraint.up, Vector3.up).normalized, Time.deltaTime * turnSmoothness);
 
         head.Update();
         leftHand.Update();
